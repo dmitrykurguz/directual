a018c1268ec3e7e516bda249f62a5802afedb3b4
onexdrk <onexdrk@gmail.com>
2017-11-10 16:00:33 +0300

    test

--- behave/cases/features/steps/step_crud.py
+++ behave/cases/features/steps/step_crud.py
@@ -11,19 +11,18 @@ from step_utils  import readCookieFromCache, appAddress, paramFromConfig, debug
 
 @given('отправляем авторизованный запрос на "{path}"')
 def step_impl(context, path):
-  sessionid = readCookieFromCache("sessionid")
-  
-  assert sessionid is not None
-
-
-  assert False
-  
-  uri = '%s%s?%s=%s' % (appAddress(context), path, 'sessionid', sessionid['value'])
-  debug('request %s with sessionId' % uri)
-  
-  r = requests.post(uri, data=context.text)
-  handleResponse(r)
-  
+  try:
+    sessionid = readCookieFromCache("sessionid")
+    assert sessionid is not None
+    
+    uri = '%s%s?%s=%s' % (appAddress(context), path, 'sessionid', sessionid['value'])
+    debug('request %s with sessionId' % uri)
+    
+    r = requests.post(uri, data=context.text)
+    handleResponse(r)
+  except Exception:
+    debug('Unknown error', sys.exc_info)
+    assert False
   
 @when('сохраняем объект структуры "{struct_name}"')
 def step_impl(context, struct_name):
@@ -78,29 +77,33 @@ def step_impl(context, struct_name, id):
 
 @then('объект структуры "{struct_name}" имеет поля')
 def step_impl(context, struct_name):
-  app_id = paramFromConfig(context, 'app_id')
-  app_secret = paramFromConfig(context, 'app_secret')
-  
-  step_params = json.loads(context.text)
-  filters = step_params['filter']
-  assertion = flatten_json(step_params['assert'])
+  try:
+    app_id = paramFromConfig(context, 'app_id')
+    app_secret = paramFromConfig(context, 'app_secret')
+    
+    step_params = json.loads(context.text)
+    filters = step_params['filter']
+    assertion = flatten_json(step_params['assert'])
 
-  uri = '%s/good/api/v3/struct/%s/search/?appID=%s&appSecret=%s' % (appAddress(context), struct_name, app_id, app_secret)  
-  debug('looking %s with filters: %s' % (struct_name, filters))
-  debug('call: %s' % uri)
-  r = requests.post(uri, data=json.dumps(filters))
-  handleResponse(r)
+    uri = '%s/good/api/v3/struct/%s/search/?appID=%s&appSecret=%s' % (appAddress(context), struct_name, app_id, app_secret)  
+    debug('looking %s with filters: %s' % (struct_name, filters))
+    debug('call: %s' % uri)
+    r = requests.post(uri, data=json.dumps(filters))
+    handleResponse(r)
 
-  result = json.loads(r.text)
-  resultList = result['result']['list']
-  assert len(resultList) == 1
-  item = resultList[0]['obj']
-  debug('item = %s' % item)
-  debug(assertion)
+    result = json.loads(r.text)
+    resultList = result['result']['list']
+    assert len(resultList) == 1
+    item = resultList[0]['obj']
+    debug('item = %s' % item)
+    debug(assertion)
 
-  flattenItem = flatten_json(item)
-  for assertKey, assertValue in assertion.items():
-    assert flattenItem[assertKey] == assertValue
+    flattenItem = flatten_json(item)
+    for assertKey, assertValue in assertion.items():
+      assert flattenItem[assertKey] == assertValue
+  except Exception:
+    debug('Unknown error', sys.exc_info)
+    assert False
 
 
 def handleResponse(r):
